9d8de0a3d87d1ee031892816b922d7af4487787c
KristhDev <kristhdev@gmail.com>
2024-04-24 16:59:19 -0600

    Feat: update sections projects

--- scripts/src/actions/get-last-projects.js
+++ scripts/src/actions/get-last-projects.js
@@ -11,7 +11,7 @@ const getLastProjects = async () => {
 
             template += `      <img alt="${ project.name }" src="${ project.image }" /> \n`;
             template += `      <h3 align="center">${ project.name }</h3> \n`;
-            template += '      <p align="left">' + (project.description.length > 300 ? project.description.substring(0, 300) + '...' : project.description) + '</p> \n';
+            template += '      <p align="left">' + (project.description.length > 250 ? project.description.substring(0, 250) + '...' : project.description) + '</p> \n';
 
             template += '      <div> \n';
 
