a89252460a10f6e0553c25fca726d7b0ac397835
KristhDev <kristhdev@gmail.com>
2025-01-08 16:18:02 -0600

    Feat(config): add PORTFOLIO_BANNER_URL to environment configuration

--- scripts/src/config/env.ts
+++ scripts/src/config/env.ts
@@ -5,5 +5,6 @@ import { get } from 'env-var';
 
 export const env = {
     PORTFOLIO_API_URL: get('PORTFOLIO_API_URL').required().asString(),
-    PORTFOLIO_URL: get('PORTFOLIO_URL').required().asString()
+    PORTFOLIO_BANNER_URL: get('PORTFOLIO_BANNER_URL').required().asString(),
+    PORTFOLIO_URL: get('PORTFOLIO_URL').required().asString(),
 }
