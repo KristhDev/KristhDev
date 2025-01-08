521375bb0c2a462492bf33be12bc6b2fa66097b4
KristhDev <kristhdev@gmail.com>
2025-01-08 15:41:02 -0600

    Feat(constants): update markers constants and messages for banner section

--- scripts/src/application/constants/markers.constant.ts
+++ scripts/src/application/constants/markers.constant.ts
@@ -1,8 +1,11 @@
 export const markers = {
-    PROJECTS_START: '<!-- LAST_PROJECTS:START -->',
+    BANNER_SECTION_END: '<!-- BANNER_SECTION:END -->',
+    BANNER_SECTION_START: '<!-- BANNER_SECTION:START -->',
     PROJECTS_END: '<!-- LAST_PROJECTS:END -->',
+    PROJECTS_START: '<!-- LAST_PROJECTS:START -->',
 }
 
 export const markersMessages = {
+    BANNER_MAKERS_FAILED: 'Los marcadores de banner no fueron encontrados o estan mal configurados.',
     PROJECT_MAKERS_FAILED: 'Los marcadores de proyectos no fueron encontrados o estan mal configurados.',
 }
