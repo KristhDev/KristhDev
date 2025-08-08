b11d81308378777bf04137c2c8a807d1d8ee1c5a
KristhDev <kristhdev@gmail.com>
2025-08-08 02:02:00 -0600

    Refactor(scripts): simplify README update sections

--- scripts/src/scripts/update-banner-section.ts
+++ scripts/src/scripts/update-banner-section.ts
@@ -1,11 +1,4 @@
-/* Constants */
-import { markers, markersMessages } from '../application/constants/markers.constant';
-
-/* Errors */
-import { MarkdownError } from '../domain/errors/mardown.error';
-
-/* Services */
-import { ReadmeService } from '../infrastructure/services/readme.service';
+import { updateBannerSectionOfReadmeUseCase } from '../config/di';
 
 /**
  * Updates the banner section in the README.md file.
@@ -17,27 +10,7 @@ import { ReadmeService } from '../infrastructure/services/readme.service';
  */
 const updateBannerSection = async (): Promise<void> => {
     try {
-        const bannerSection = ReadmeService.generateBannerSection();
-        const readmeContent = ReadmeService.loadReadme();
-
-        const startIndex = readmeContent.indexOf(markers.BANNER_SECTION_START);
-        const endIndex = readmeContent.indexOf(markers.BANNER_SECTION_END);
-
-        const hasStartMarker = startIndex !== -1;
-        const hasEndMarker = endIndex !== -1;
-
-        const hasMarkers = hasStartMarker && hasEndMarker;
-
-        const areMarkersWellPositioned = hasMarkers && ((startIndex + markers.BANNER_SECTION_START.length) < endIndex);
-        if (!hasMarkers || !areMarkersWellPositioned) throw new MarkdownError(markersMessages.BANNER_MAKERS_FAILED);
-
-        const updatedReadmeContent = ReadmeService.updateSection({
-            content: readmeContent,
-            markers: { start: startIndex + markers.BANNER_SECTION_START.length, end: endIndex },
-            newContent: bannerSection
-        });
-
-        ReadmeService.updateReadme(updatedReadmeContent);
+        updateBannerSectionOfReadmeUseCase.execute();
     }
     catch (error) {
         console.error(error);
--- scripts/src/scripts/update-last-projects.ts
+++ scripts/src/scripts/update-last-projects.ts
@@ -1,12 +1,4 @@
-/* Constants */
-import { markers, markersMessages } from '../application/constants/markers.constant';
-
-/* Errors */
-import { MarkdownError } from '../domain/errors/mardown.error';
-
-/* Services */
-import { ReadmeService } from '../infrastructure/services/readme.service';
-import { ProjectsService } from '../infrastructure/services/projects.service';
+import { updateLastProjectsSectionOfReadmeUseCase } from '../config/di';
 
 /**
  * Updates the projects section in the README.md file.
@@ -18,33 +10,7 @@ import { ProjectsService } from '../infrastructure/services/projects.service';
  */
 const updateLastProjects = async (): Promise<void> => {
     try {
-        const projects = await ProjectsService.getLatest();
-        const projectsTableRows = ReadmeService.generateProjectsTableRows(projects);
-
-        let projectsTable = '<table align="center"> \n';
-        projectsTable += projectsTableRows;
-        projectsTable += '</table>';
-
-        const readmeContent = ReadmeService.loadReadme();
-
-        const startIndex = readmeContent.indexOf(markers.PROJECTS_START);
-        const endIndex = readmeContent.indexOf(markers.PROJECTS_END);
-
-        const hasStartMarker = startIndex !== -1;
-        const hasEndMarker = endIndex !== -1;
-
-        const hasMarkers = hasStartMarker && hasEndMarker;
-
-        const areMarkersWellPositioned = hasMarkers && ((startIndex + markers.PROJECTS_START.length) < endIndex);
-        if (!hasMarkers || !areMarkersWellPositioned) throw new MarkdownError(markersMessages.PROJECT_MAKERS_FAILED);
-
-        const updatedReadmeContent = ReadmeService.updateSection({
-            content: readmeContent,
-            markers: { start: startIndex + markers.PROJECTS_START.length, end: endIndex },
-            newContent: projectsTable
-        });
-
-        ReadmeService.updateReadme(updatedReadmeContent);
+        await updateLastProjectsSectionOfReadmeUseCase.execute();
     }
     catch (error) {
         console.error(error);
