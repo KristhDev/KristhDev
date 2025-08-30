1caca92e9933e33018f5248889a179b2e41a5214
KristhDev <kristhdev@gmail.com>
2025-08-29 23:37:39 -0600

    feat(scripts): add script to update readme skills
    Automates the process of keeping the skills section in the README up-to-date.

---
+++ scripts/src/scripts/update-skills-section.ts
@@ -0,0 +1,19 @@
+import { updateSkillsSectionOfReadmeUseCase } from '../config/di';
+
+/**
+ * Updates the skills section in the README.md file.
+ *
+ * The skills section is the section that contains the skills of the user.
+ *
+ * @returns {Promise<void>} A Promise that resolves when the skills section has been updated.
+ */
+const updateSkillsSection = async (): Promise<void> => {
+    try {
+        await updateSkillsSectionOfReadmeUseCase.execute();
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+updateSkillsSection();
