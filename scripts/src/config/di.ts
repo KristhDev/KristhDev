ed8bce7c64e55bb04727b815069cb98bf87dc8aa
KristhDev <kristhdev@gmail.com>
2025-08-29 23:37:53 -0600

    feat(di): configure skills section update

--- scripts/src/config/di.ts
+++ scripts/src/config/di.ts
@@ -1,8 +1,9 @@
+import { SkillsService } from './../infrastructure/services/skills.service';
 /* Contracts */
 import { ApiAdapterContract, FileSystemAdapterContract } from '../domain/contracts/adapters';
-import { MarkdownServiceContract, ProjectsServiceContract, ReadmeServiceContract } from '../domain/contracts/services';
+import { MarkdownServiceContract, ProjectsServiceContract, ReadmeServiceContract, SkillsServiceContract } from '../domain/contracts/services';
 import { ReadmeFacadeContract } from '../domain/contracts/facades';
-import { UpdateBannerSectionOfReadmeUseCaseContract, UpdateLastProjectsSectionOfReadmeUseCaseContract } from '../domain/contracts/usecases';
+import { UpdateBannerSectionOfReadmeUseCaseContract, UpdateLastProjectsSectionOfReadmeUseCaseContract, UpdateSkillsSectionOfReadmeUseCaseContract } from '../domain/contracts/usecases';
 
 /* Adapters */
 import { ApiAdapter, FileSystemAdapter } from '../infrastructure/adapters';
@@ -14,16 +15,18 @@ import { MarkdownService, ProjectsService, ReadmeService } from '../infrastructu
 import { ReadmeFacade } from '../application/facades';
 
 /* UseCases */
-import { UpdateBannerSectionOfReadmeUseCase, UpdateLastProjectsSectionOfReadmeUseCase } from '../application/usecases';
+import { UpdateBannerSectionOfReadmeUseCase, UpdateLastProjectsSectionOfReadmeUseCase, UpdateSkillsSectionOfReadmeUseCase } from '../application/usecases';
 
 export const apiAdapter: ApiAdapterContract = new ApiAdapter();
 export const fileSystemAdapter: FileSystemAdapterContract = new FileSystemAdapter();
 
-export const projectsService: ProjectsServiceContract = new ProjectsService(apiAdapter);
 export const markdownService: MarkdownServiceContract = new MarkdownService(fileSystemAdapter);
+export const projectsService: ProjectsServiceContract = new ProjectsService(apiAdapter);
 export const readmeService: ReadmeServiceContract= new ReadmeService(markdownService);
+export const skillsService: SkillsServiceContract = new SkillsService(apiAdapter);
 
-export const readmeFacade: ReadmeFacadeContract = new ReadmeFacade(projectsService, readmeService);
+export const readmeFacade: ReadmeFacadeContract = new ReadmeFacade(projectsService, skillsService, readmeService);
 
 export const updateBannerSectionOfReadmeUseCase: UpdateBannerSectionOfReadmeUseCaseContract = new UpdateBannerSectionOfReadmeUseCase(readmeFacade);
 export const updateLastProjectsSectionOfReadmeUseCase: UpdateLastProjectsSectionOfReadmeUseCaseContract = new UpdateLastProjectsSectionOfReadmeUseCase(readmeFacade);
+export const updateSkillsSectionOfReadmeUseCase: UpdateSkillsSectionOfReadmeUseCaseContract = new UpdateSkillsSectionOfReadmeUseCase(readmeFacade);
