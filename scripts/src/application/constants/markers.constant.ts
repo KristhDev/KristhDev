64a20c2dfb1ab554ada05aae84923c7e413a6708
KristhDev <kristhdev@gmail.com>
2025-08-29 23:30:07 -0600

    feat(constants): add skill section content markers
    Introduces new markers for the start and end of a skills section, along with a corresponding error message. This supports the processing and dynamic generation of a dedicated skills section.

--- scripts/src/application/constants/markers.constant.ts
+++ scripts/src/application/constants/markers.constant.ts
@@ -3,9 +3,12 @@ export const markers = {
     BANNER_SECTION_START: '<!-- BANNER_SECTION:START -->',
     PROJECTS_END: '<!-- LAST_PROJECTS:END -->',
     PROJECTS_START: '<!-- LAST_PROJECTS:START -->',
+    SKILLS_END: '<!-- SKILLS_SECTION:END -->',
+    SKILLS_START: '<!-- SKILLS_SECTION:START -->',
 }
 
 export const markersMessages = {
     BANNER_MAKERS_FAILED: 'Los marcadores de banner no fueron encontrados o estan mal configurados.',
     PROJECT_MAKERS_FAILED: 'Los marcadores de proyectos no fueron encontrados o estan mal configurados.',
+    SKILL_MAKERS_FAILED: 'Los marcadores de habilidades no fueron encontrados o estan mal configurados.',
 }
