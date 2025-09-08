f53bc51ee0fcd29a4efbb0c4817d1511f5918051
KristhDev <kristhdev@gmail.com>
2025-09-08 12:17:16 -0600

    feat(di): add logger adapter

--- scripts/src/config/di.ts
+++ scripts/src/config/di.ts
@@ -1,15 +1,14 @@
-import { SkillsService } from '@infrastructure/services/skills.service';
 /* Contracts */
-import { ApiAdapterContract, FileSystemAdapterContract } from '@domain/contracts/adapters';
+import { ApiAdapterContract, FileSystemAdapterContract, LoggerAdapterContract } from '@domain/contracts/adapters';
 import { MarkdownServiceContract, ProjectsServiceContract, ReadmeServiceContract, SkillsServiceContract } from '@domain/contracts/services';
 import { ReadmeFacadeContract } from '@domain/contracts/facades';
 import { UpdateBannerSectionOfReadmeUseCaseContract, UpdateLastProjectsSectionOfReadmeUseCaseContract, UpdateSkillsSectionOfReadmeUseCaseContract } from '@domain/contracts/usecases';
 
 /* Adapters */
-import { ApiAdapter, FileSystemAdapter } from '@infrastructure/adapters';
+import { ApiAdapter, FileSystemAdapter, LoggerAdapter } from '@infrastructure/adapters';
 
 /* Services */
-import { MarkdownService, ProjectsService, ReadmeService } from '@infrastructure/services';
+import { MarkdownService, ProjectsService, ReadmeService, SkillsService } from '@infrastructure/services';
 
 /* Facades */
 import { ReadmeFacade } from '@application/facades';
@@ -19,6 +18,7 @@ import { UpdateBannerSectionOfReadmeUseCase, UpdateLastProjectsSectionOfReadmeUs
 
 export const apiAdapter: ApiAdapterContract = new ApiAdapter();
 export const fileSystemAdapter: FileSystemAdapterContract = new FileSystemAdapter();
+export const loggerAdapter: LoggerAdapterContract = new LoggerAdapter({ renderLogsInConsole: true, writeLogsInFile: true }); 
 
 export const markdownService: MarkdownServiceContract = new MarkdownService(fileSystemAdapter);
 export const projectsService: ProjectsServiceContract = new ProjectsService(apiAdapter);
