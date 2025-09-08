e781f0b3aa72510af68cedcfdba17abc2b0853df
KristhDev <kristhdev@gmail.com>
2025-09-08 16:37:07 -0600

    refactor(contracts): make social media section update async
    Updates the `updateSocialMediaSection` method signature to return a `Promise<void>`. This change indicates that the method now performs asynchronous operations, allowing for proper handling of tasks that require waiting.

--- scripts/src/domain/contracts/facades/readme.facade.contract.ts
+++ scripts/src/domain/contracts/facades/readme.facade.contract.ts
@@ -1,6 +1,6 @@
 export abstract class ReadmeFacadeContract {
     public abstract updateBannerSection(): void;
-    public abstract updateSocialMediaSection(): void;
+    public abstract updateSocialMediaSection(): Promise<void>;
     public abstract updateLastProjectsSection(): Promise<void>;
     public abstract updateSkillsSection(): Promise<void>;
 }
