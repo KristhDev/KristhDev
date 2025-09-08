fad852fce47193a32e7ca00624588d703e473537
KristhDev <kristhdev@gmail.com>
2025-09-08 16:04:30 -0600

    feat(constants): add social media section markers
    Adds new constant markers and an associated error message to support the dynamic generation or update of a social media section. This allows for clear demarcation and validation of the social media content area.

--- scripts/src/application/constants/markers.constant.ts
+++ scripts/src/application/constants/markers.constant.ts
@@ -1,6 +1,8 @@
 export const markers = {
     BANNER_SECTION_END: '<!-- BANNER_SECTION:END -->',
     BANNER_SECTION_START: '<!-- BANNER_SECTION:START -->',
+    SOCIAL_MEDIA_SECTION_END: '<!-- SOCIAL_MEDIA_SECTION:END -->',
+    SOCIAL_MEDIA_SECTION_START: '<!-- SOCIAL_MEDIA_SECTION:START -->',
     PROJECTS_END: '<!-- LAST_PROJECTS:END -->',
     PROJECTS_START: '<!-- LAST_PROJECTS:START -->',
     SKILLS_END: '<!-- SKILLS_SECTION:END -->',
@@ -9,6 +11,7 @@ export const markers = {
 
 export const markersMessages = {
     BANNER_MAKERS_FAILED: 'Los marcadores de banner no fueron encontrados o estan mal configurados.',
+    SOCIAL_MEDIA_MAKERS_FAILED: 'Los marcadores de redes sociales no fueron encontrados o estan mal configurados.',
     PROJECT_MAKERS_FAILED: 'Los marcadores de proyectos no fueron encontrados o estan mal configurados.',
     SKILL_MAKERS_FAILED: 'Los marcadores de habilidades no fueron encontrados o estan mal configurados.',
 }
