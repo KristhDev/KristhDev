22a681a25de73914ece2002c2ddd9b31fcf16a65
KristhDev <kristhdev@gmail.com>
2025-09-08 16:05:10 -0600

    feat(contracts): add social media section update

--- scripts/src/domain/contracts/facades/readme.facade.contract.ts
+++ scripts/src/domain/contracts/facades/readme.facade.contract.ts
@@ -1,5 +1,6 @@
 export abstract class ReadmeFacadeContract {
     public abstract updateBannerSection(): void;
+    public abstract updateSocialMediaSection(): void;
     public abstract updateLastProjectsSection(): Promise<void>;
     public abstract updateSkillsSection(): Promise<void>;
 }
