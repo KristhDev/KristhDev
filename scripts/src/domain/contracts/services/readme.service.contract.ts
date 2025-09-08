34b064afadbac3574eb333b9097bef66be5f056b
KristhDev <kristhdev@gmail.com>
2025-09-08 16:04:49 -0600

    feat(readme): add social media section generation
    Adds a new abstract method to the ReadmeServiceContract to support generating a dedicated social media section within the README file.

--- scripts/src/domain/contracts/services/readme.service.contract.ts
+++ scripts/src/domain/contracts/services/readme.service.contract.ts
@@ -5,6 +5,7 @@ import { UpdateMarkdownSectionOptions } from '@infrastructure/interfaces';
 export abstract class ReadmeServiceContract {
     public abstract loadReadme(path?: string): string;
     public abstract generateBannerSection(): string;
+    public abstract generateSocialMediaSection(): string;
     public abstract generateProjectsTableRows(projects: ProjectEntity[]): string;
     public abstract generateSkillsSection(skills: SkillEntity[]): string;
     public abstract updateReadme(content: string, path?: string): void;
